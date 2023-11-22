25809e42893de75f11e15d29f6c1d79c13a771c7
mdorizon <mdorizon17@gmail.com>
2023-11-22 13:54:59 +0100

    scss

---
+++ src/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Signin</a>
+        </li>
+        <li>
+          <a href="">Account</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+  <section>
+    <h1>My amazing blog</h1>
+
+    <div class="row">
+      <div class="col-md-3">
+        <div class="card">
+          <img src="https://source.unsplash.com/random/300x300/?pasta" alt="">
+          <div class="card-body">
+            <h2>Amazin Card Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="https://source.unsplash.com/random/300x300" alt="">
+          <div class="card-body">
+            <h2>Amazin Card Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="https://source.unsplash.com/random/300x300" alt="">
+          <div class="card-body">
+            <h2>Amazin Card Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="https://source.unsplash.com/random/300x300" alt="">
+          <div class="card-body">
+            <h2>Amazin Card Title</h2>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
