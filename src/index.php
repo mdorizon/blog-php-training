e700ae8595a7cd0961ffe5354bf68e5bab61dc32
mdorizon <mdorizon17@gmail.com>
2023-11-22 14:37:13 +0100

    header-php & scss multi file

--- src/index.php
+++ src/index.php
@@ -1,72 +1,66 @@
-<!DOCTYPE html>
-<html lang="fr">
+<?php
+  var_dump(__DIR__);
+  require_once 'header.php';
+?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./style.css">
-  <title>Document</title>
-</head>
+<header>
+  <nav>
+    <ul>
+      <li>
+        <a href="">Home</a>
+      </li>
+      <li>
+        <a href="">Signin</a>
+      </li>
+      <li>
+        <a href="">Account</a>
+      </li>
+    </ul>
+  </nav>
+</header>
 
-<body>
-  <header>
-    <nav>
-      <ul>
-        <li>
-          <a href="">Home</a>
-        </li>
-        <li>
-          <a href="">Signin</a>
-        </li>
-        <li>
-          <a href="">Account</a>
-        </li>
-      </ul>
-    </nav>
-  </header>
+<section>
+  <h1>My amazing blog</h1>
 
-  <section>
-    <h1>My amazing blog</h1>
-
-    <div class="row">
-      <div class="col-md-3">
-        <div class="card">
-          <img src="https://source.unsplash.com/random/300x300/?pasta" alt="">
-          <div class="card-body">
-            <h2>Amazin Card Title</h2>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="card">
-          <img src="https://source.unsplash.com/random/300x300" alt="">
-          <div class="card-body">
-            <h2>Amazin Card Title</h2>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="card">
-          <img src="https://source.unsplash.com/random/300x300" alt="">
-          <div class="card-body">
-            <h2>Amazin Card Title</h2>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="card">
-          <img src="https://source.unsplash.com/random/300x300" alt="">
-          <div class="card-body">
-            <h2>Amazin Card Title</h2>
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
-          </div>
+  <div class="row">
+    <div class="col-md-3">
+      <div class="card">
+        <img src="https://source.unsplash.com/random/300x300" alt="">
+        <div class="card-body">
+          <h2>Amazin Card Title</h2>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
         </div>
       </div>
     </div>
-  </section>
+    <div class="col-md-3">
+      <div class="card">
+        <img src="https://source.unsplash.com/random/300x300" alt="">
+        <div class="card-body">
+          <h2>Amazin Card Title</h2>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card">
+        <img src="https://source.unsplash.com/random/300x300" alt="">
+        <div class="card-body">
+          <h2>Amazin Card Title</h2>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card">
+        <img src="https://source.unsplash.com/random/300x300" alt="">
+        <div class="card-body">
+          <h2>Amazin Card Title</h2>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 </body>
 
 </html>
